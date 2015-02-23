21d943bb58dc7e9b0aa7d4118bf3c9b162125abe
Amit jaiswal <amitjaiswal396@gmail.com>
2015-02-23 15:47:48 +0530

    step.py changes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'emailvalidator',
   packages = ['emailvalidator'], # this must be the same as the name above
-  version = '1.0',
+  version = '2.0',
   description = 'A python package for validating email',
   author = 'Amit Jaiswal, Rohit Sonawane',
   author_email = 'amitjaiswal396@gmail.com, rohit',
   url = 'https://github.com/amitjaiswal396/emailvalidator', # use the URL to the github repo
-  download_url = 'https://github.com/amitjaiswal396/emailvalidator/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/amitjaiswal396/emailvalidator/tarball/0.2', # I'll explain this in a second
   keywords = ['email', 'email validation', 'email validator', 'python package for email validation', 'python email validator'], # arbitrary keywords
   classifiers = ['Natural Language :: English','License :: Freeware', 'Programming Language :: Python :: 2.7'],
 )
