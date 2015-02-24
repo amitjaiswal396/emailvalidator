5fd757eb8a05e7c36be0cb30023fff1236916057
Amit jaiswal <amitjaiswal396@gmail.com>
2015-02-24 16:25:33 +0530

    dffds

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
   author_email = 'amitjaiswal396@gmail.com, rohitsonawane.genie@gmail.com',
   url = 'https://github.com/amitjaiswal396/emailvalidator', # use the URL to the github repo
   download_url = 'https://github.com/amitjaiswal396/emailvalidator/tarball/0.3', # I'll explain this in a second
-  keywords = ['email', 'email validation', 'email validator', 'python package for email validation', 'python email validator'], # arbitrary keywords
+  keywords = ['emailvalidator python','python emailvalidator','email', 'email validation', 'email validator', 'python package for email validation', 'python email validator'], # arbitrary keywords
   classifiers = ['Natural Language :: English','License :: Freeware', 'Programming Language :: Python :: 2.7'],
 )
